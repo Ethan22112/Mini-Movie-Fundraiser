49a1df182d14cb655d58bea2ae3ed1c6bae75554
Ethan22112 <22112@fraser.school.nz>
2025-04-08 10:06:45 +1200

    Update Looping
    Made Some Changes To The Program As Variable 'Tickets_Sold' was able to exceed variable 'MAX_TICKETS'

--- Looping.py
+++ Looping.py
@@ -3,7 +3,7 @@ MAX_TICKETS = 5
 Tickets_Sold = 0
 
 #continues executing the code below the while loop, while Variable Tickets_Sold
-while Tickets_Sold <= MAX_TICKETS:
+while Tickets_Sold < MAX_TICKETS:
     #Initializing Error Code
     Error_Code = 'xxx'
     name = input("Please Enter Your Name: ")
@@ -16,7 +16,7 @@ while Tickets_Sold <= MAX_TICKETS:
     Tickets_Sold += 1
 
 #Checks Whether the amount of 'Tickets_Sold' has exceeded, or matched the value in MAX_TICKETS
-if Tickets_Sold >= MAX_TICKETS:
+if Tickets_Sold == MAX_TICKETS:
     print(f'You have sold all of the tickets, you have {Tickets_Sold} / {MAX_TICKETS} tickets sold')
 else:
     print(f'You have sold {Tickets_Sold} / {MAX_TICKETS} tickets')
