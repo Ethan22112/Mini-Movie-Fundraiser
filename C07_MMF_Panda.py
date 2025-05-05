71d4ca912e2a2201e035ae36dd8b357fcfb7ae02
Ethan22112 <22112@fraser.school.nz>
2025-05-06 10:07:42 +1200

    Making MMF panda hard coded
    using package panda to print a table

---
+++ C07_MMF_Panda.py
@@ -0,0 +1,19 @@
+import pandas
+
+#lists to store ticket details
+
+all_names = ["A", "B", "C", "D", "E"]
+all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
+all_ticket_surcharges = [0, 0, 0.53, 0.53, 0]
+
+mini_movie_dict = {
+    'Name': all_names,
+    'Ticket_Price': all_ticket_costs,
+    'Ticket_Surcharge': all_ticket_surcharges
+}
+
+#create data frame / table from dictionary
+
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+
+print(mini_movie_frame)
